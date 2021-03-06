a2cf2071462a98809dae510608b39bb0e198bb81
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2021-03-06 20:05:32 +0200

    Added HttpsRedirect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import "./App.css";
-import Donate from "./Components/Donate/Donate";
+import HttpsRedirect from "react-https-redirect";
 import Footer from "./Components/Footer/Footer";
-import MainText from "./Components/MainText/MainText";
 import Navbar from "./Components/Navbar/Navbar";
-import OurServers from "./Components/OurServers/OurServers";
-import Posts from "./Components/Posts/Posts";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <div className="wrapper">
-          <Navbar />
-          <Route path="/" component={Home} exact></Route>
-          <Footer />
+    <HttpsRedirect>
+      <BrowserRouter>
+        <div className="App">
+          <div className="wrapper">
+            <Navbar />
+            <Route path="/" component={Home} exact></Route>
+            <Footer />
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </HttpsRedirect>
   );
 }
 
